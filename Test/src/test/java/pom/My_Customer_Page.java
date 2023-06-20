96c61ad93487a31b117cfcb37b782707e04454d1
shubhamthere347 <120366537+shubhamthere347@users.noreply.github.com>
2023-06-20 22:17:18 +0530

    Update My_Customer_Page.java

--- Test/src/test/java/pom/My_Customer_Page.java
+++ Test/src/test/java/pom/My_Customer_Page.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 public class My_Customer_Page {
 
 	@FindBy(xpath="//span[text()=\" New Customer \"]") private WebElement innewcust;
-	
-	//
+
 	public My_Customer_Page(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
